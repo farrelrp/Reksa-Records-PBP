03f521f6f676630217f014a62a2f25f21f54feea
farrelreksa <farrelreksa@gmail.com>
2024-09-16 17:21:53 +0700

    Tugas 3 Progress: Completed form, navbar and logo. Models and views for json and xml also done.

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import VinylRecord
+from .forms import VinylRecordForm
 from django.http import HttpResponse
 from django.core import serializers
 
@@ -16,12 +17,34 @@ def show_main(request):
     return render(request, "main.html", context)
 
 def create_vinyl(request):
-    form = VinylRecordForm(request.POST or None)
+    form = VinylRecordForm(request.POST or None, request.FILES or None)
 
     if form.is_valid():
         form.save()
         return redirect('main:show_main')
     
-    context = {'form' : form}
+    context = {'form' : form,
+               'aplikasi' : "Reksa's Records"}
     
     return render(request, "create_vinyl.html", context)
+
+def show_xml(request):
+    vinyls = VinylRecord.objects.all()
+    data = serializers.serialize('xml', vinyls)
+    return HttpResponse(data, content_type='application/xml')
+
+def show_json(request):
+    vinyls = VinylRecord.objects.all()
+    data = serializers.serialize('json', vinyls)
+    return HttpResponse(data, content_type='application/json')
+
+def show_xml_by_id(request, id):
+    vinyls = VinylRecord.objects.filter(id=id)
+    data = serializers.serialize('xml', vinyls)
+    return HttpResponse(data, content_type='application/xml')
+
+def show_json_by_id(request, id):
+    vinyls = VinylRecord.objects.filter(id=id)
+    data = serializers.serialize('json', vinyls)
+    return HttpResponse(data, content_type='application/json')
+
