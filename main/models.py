576d49c8b09cedadff30acfa6056236d8676871b
farrelrp_ <farrelreksa@gmail.com>
2024-09-20 22:04:29 +0700

    Tugas 4 Progress: Added Login Page, Register Page and Tied User with Items

--- main/models.py
+++ main/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 import uuid
 
 # Create your models here.
 class VinylRecord(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     image = models.ImageField(upload_to='images/' , default='images/3b921c51dc99d9fb2be192af3ec14f72.jpg')
     album_name = models.CharField(max_length=100)
