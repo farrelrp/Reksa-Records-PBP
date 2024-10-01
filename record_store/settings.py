02285ad8dca52309000b6d15be1660109c52eca1
farrelrp_ <farrelreksa@gmail.com>
2024-10-01 13:29:46 +0700

    Tugas 5 Progress: Static files

--- record_store/settings.py
+++ record_store/settings.py
@@ -121,11 +121,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+if DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static' # merujuk ke /static root project pada mode development
+    ]
+else:
+    STATIC_ROOT = BASE_DIR / 'static' # merujuk ke /static root project pada mode production
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # Points to project-level static directory
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
